3a33a452f107c7c39ff99f70d12f7328fd9cf853
Sharma <687400@cognizant.com>
2018-10-15 11:33:13 +0530

    Updating the Js code

--- 10HoldShiftToSelectMultiple/script.js
+++ 10HoldShiftToSelectMultiple/script.js
@@ -1,1 +1,29 @@
- document.querySelectorAll(' input[type="checkbox"]').multiple = true;
+var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+var checkboxArray = Array.from(checkboxes);
+console.log(checkboxArray);
+
+
+var lastChecked;
+
+
+function handleCheck(e) {
+    console.log(e);
+    var inBetween = false;
+    if (e.shiftKey && this.checked) {
+        console.log(this);
+        checkboxArray.forEach(checkbox => {
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+            }
+
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        });
+    }
+    lastChecked = this;
+}
+checkboxArray.forEach(checkbox => {
+    addEventListener('click', handleCheck)
+});
